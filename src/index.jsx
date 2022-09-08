915b13dc5c4cc709dc79c9ec3fae2294a08f66d5
stefanofiorenzaknits <stefano.fiorenza.knits@gmail.com>
2022-09-08 19:37:05 +0300

    [S4-Redux-Synch]: removed spinner css from index

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,7 @@ import "./assets/vendor/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fullcalendar/common/main.min.css";
 import "@fullcalendar/daygrid/main.min.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import "react-notification-alert/dist/animate.css";
 import "sweetalert2/dist/sweetalert2.min.css";
